39865ac6262e77d0713f7917dfabb3334da6044f
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-09-17 12:33:04 -0700

    upadate the position of content

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const ProjectCard = ({project}) => {
+const ProjectCard = ({ project }) => {
   return (
     <div className="projects item">
+      <div className="projects media">
         <div className="projects image">{project.image}</div>
         <div className="projects demo">{project.demo}</div>
+      </div>
+      <div className="projects icon">
+        <span className="project action-icon">
+          <i className="uil uil-play"></i>
+        </span>
+        <span className="project action-icon">
+          <i className="uil uil-pause"></i>
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,35 +13,58 @@ const Projects = () => {
   };
   const project2 = {
     title: "Animal Crossing Matching",
-    description:"Animal Crossing Matching is a matching game to improve a  user's memory skills.",
+    description:
+      "Animal Crossing Matching is a matching game to improve a  user's memory skills.",
     image: "",
     demo: "https://youtu.be/-8mJt1CO7Mo",
     github: "https://github.com/RRZhai/Animal_Crossing_Matching",
-    website: "https://rrzhai.github.io/Animal_Crossing_Matching/"
+    website: "https://rrzhai.github.io/Animal_Crossing_Matching/",
   };
   const project3 = {
     title: "Haiku Heaven",
-    description:"Haiku Heaven is a creative platform empowering users to compose and appreciate Haikus",
+    description:
+      "Haiku Heaven is a creative platform empowering users to compose and appreciate Haikus",
     image: "",
     demo: "https://youtu.be/a0xsgQhFos0",
     github: "https://github.com/RRZhai/Haiku_Heaven",
-    website: "https://haikuhaven.onrender.com/"
+    website: "https://haikuhaven.onrender.com/",
   };
-  const [selectproject, setSelectProject] = useState(project1)
+  const [selectproject, setSelectProject] = useState(project1);
   const handleSelectProject = (project) => {
-    setSelectProject(project)
-  }
+    setSelectProject(project);
+  };
   return (
     <section className="projects section" id="projects">
       <div className="project_container container grid">
         <div className="project content">
           <h1 className="project title">{selectproject.title}</h1>
           <p className="project description">{selectproject.description}</p>
+          <a
+            href={selectproject.website}
+            className="button button--flex"
+            target="_blank"
+          >
+            Wanna try?
+            <i class="uil uil-arrow-circle-right button_icon"></i>
+          </a>
         </div>
         <div className="projects slide">
-          <ProjectCard project={project1} handleSelectProject={handleSelectProject}/>
-          <ProjectCard project={project2} handleSelectProject={handleSelectProject} />
-          <ProjectCard project={project3} handleSelectProject={handleSelectProject} />
+          <ProjectCard
+            project={project1}
+            handleSelectProject={handleSelectProject}
+          />
+          <ProjectCard
+            project={project2}
+            handleSelectProject={handleSelectProject}
+          />
+          <ProjectCard
+            project={project3}
+            handleSelectProject={handleSelectProject}
+          />
+        </div>
+        <div className="projects slide_icon">
+          <i class="uil uil-angle-up icon"></i>
+          <i class="uil uil-angle-down icon"></i>
         </div>
       </div>
     </section>
