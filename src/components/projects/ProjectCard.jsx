bde0c8807ce4a83884e4114d292d0884555105c4
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-09-19 00:46:19 -0700

    need fix youtube size

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,28 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProjectCard = ({ project, className }) => {
-  console.log(project, className);
+  const [play, setPlay] = useState(false);
+
   return (
     <div className="projects media card-media active">
-      <div className="projects image">
-        <img src={project.image} alt={project.title} />
-      </div>
-      <div className="projects demo">
-        <iframe
-          src={project.demo}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullscreen
-        ></iframe>
-      </div>
+      {play ? (
+        <div className="projects demo">
+          <iframe
+            src={project.demo}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullscreen
+          ></iframe>
+        </div>
+      ) : (
+        <div className="projects image">
+          <img src={project.image} alt={project.title} />
+        </div>
+      )}
+
       <div className="projects play-icon icon">
-        <i class="uil uil-play icon"></i>
-        <i class="uil uil-pause"></i>
-        <a
-          href={project.github}
-          target="_blank"
-          className="home__social-icon"
-        >
+        <i class="uil uil-play icon" onClick={() => setPlay(true)}></i>
+        <i class="uil uil-pause" onClick={() => setPlay(false)}></i>
+        <a href={project.github} target="_blank" className="home__social-icon">
           <i className="uil uil-github icon"></i>
         </a>
       </div>
