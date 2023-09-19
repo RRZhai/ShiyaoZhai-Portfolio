c9bd49ef8522df77e45efda5ec378618aee67684
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-09-19 15:30:58 -0700

    hover on the timeline

--- src/components/about/Timeline.jsx
+++ src/components/about/Timeline.jsx
@@ -1,39 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import ucbIcon from "../../assets/ucb.png";
 import cdcIcon from "../../assets/cdc.png";
 import mkaIcon from "../../assets/mka.png";
 import fsIcon from "../../assets/fs.png";
 const Timeline = () => {
+  const [showlogo, setShowlogo] = useState(false);
   return (
     <div className="timeline wrapper">
       <div className="timeline container">
         <div className="line-area"></div>
-        <div className="timeline single-time active">
-          <span>June 2019</span>
-          <div className="timeline image-area">
+        <div
+          className="timeline single-time"
+          onMouseEnter={() => setShowlogo(true)}
+          onMouseLeave={() => setShowlogo(false)}
+        >
+          <span className={`${showlogo ? "noshow" : "show"}`}>June 2019</span>
+          <div
+            className={`timeline image-area ${showlogo ? "show" : "noshow"}`}
+          >
             <img src={ucbIcon} alt="ucbIcon" />
-            <span>University of California - Berkeley</span>
+            <span>Bachelor’s Degree in Architecture</span>
           </div>
         </div>
-        <div className="timeline single-time active">
-          <span>July 2019 - Nov 2019</span>
-          <div className="timeline image-area">
+        <div
+          className="timeline single-time"
+          onMouseEnter={() => setShowlogo(true)}
+          onMouseLeave={() => setShowlogo(false)}
+        >
+          <span className={`${showlogo ? "noshow" : "show"}`}>
+            July 2019 - Nov 2019
+          </span>
+          <div
+            className={`timeline image-area ${showlogo ? "show" : "noshow"}`}
+          >
             <img src={cdcIcon} alt="cdcIcon" />
-            <span>Community Design Center</span>
+            <span>Designer</span>
           </div>
         </div>
-        <div className="timeline single-time active">
-          <span> Nov 2019 - April 2023</span>
-          <div className="timeline image-area">
+        <div
+          className="timeline single-time"
+          onMouseEnter={() => setShowlogo(true)}
+          onMouseLeave={() => setShowlogo(false)}
+        >
+          <span className={`${showlogo ? "noshow" : "show"}`}>
+            Nov 2019 - April 2023
+          </span>
+          <div
+            className={`timeline image-area ${showlogo ? "show" : "noshow"}`}
+          >
             <img src={mkaIcon} alt="mkaIcon" />
-            <span>Matano Kang Architects</span>
+            <span>Junior Architect</span>
           </div>
         </div>
-        <div className="timeline single-time active">
-          <span>April 2023 - July 2023</span>
-          <div className="timeline image-area">
+        <div
+          className="timeline single-time"
+          onMouseEnter={() => setShowlogo(true)}
+          onMouseLeave={() => setShowlogo(false)}
+        >
+          <span className={`${showlogo ? "noshow" : "show"}`}>
+            April 2023 - July 2023
+          </span>
+          <div
+            className={`timeline image-area ${showlogo ? "show" : "noshow"}`}
+          >
             <img src={fsIcon} alt="fsIcon" />
-            <span>Flatiron School</span>
+            <span>Full Stack Web Development</span>
           </div>
         </div>
       </div>
