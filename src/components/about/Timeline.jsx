71d92651b97ba543f65ce5c6bd6c2062ac2135dc
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-09-19 14:32:36 -0700

    add hover to timeline

--- src/components/about/Timeline.jsx
+++ src/components/about/Timeline.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ucbIcon from "../../assets/ucb.png";
 import cdcIcon from "../../assets/cdc.png";
 import mkaIcon from "../../assets/mka.png";
-
+import fsIcon from "../../assets/fs.png";
 const Timeline = () => {
   return (
     <div className="timeline wrapper">
@@ -11,37 +11,29 @@ const Timeline = () => {
         <div className="timeline single-time active">
           <span>June 2019</span>
           <div className="timeline image-area">
-            <a href="#">
-              <img src={ucbIcon} alt="ucbIcon" />
-              <span>University of California - Berkeley</span>
-            </a>
+            <img src={ucbIcon} alt="ucbIcon" />
+            <span>University of California - Berkeley</span>
           </div>
         </div>
         <div className="timeline single-time active">
-          <span>July 2019 - November 2019</span>
+          <span>July 2019 - Nov 2019</span>
           <div className="timeline image-area">
-            <a href="#">
-              <img src={cdcIcon} alt="cdcIcon" />
-              <span>Community Design Center</span>
-            </a>
+            <img src={cdcIcon} alt="cdcIcon" />
+            <span>Community Design Center</span>
           </div>
         </div>
         <div className="timeline single-time active">
-          <span> November 2019 - April 2023</span>
+          <span> Nov 2019 - April 2023</span>
           <div className="timeline image-area">
-            <a href="#">
-              <img src={mkaIcon} alt="mkaIcon" />
-              <span>Matano Kang Architects</span>
-            </a>
+            <img src={mkaIcon} alt="mkaIcon" />
+            <span>Matano Kang Architects</span>
           </div>
         </div>
         <div className="timeline single-time active">
           <span>April 2023 - July 2023</span>
           <div className="timeline image-area">
-            <a href="#">
-              <img src="" alt="" />
-              <span>Flatiron School</span>
-            </a>
+            <img src={fsIcon} alt="fsIcon" />
+            <span>Flatiron School</span>
           </div>
         </div>
       </div>
