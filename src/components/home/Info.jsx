1436982e52bf2753a119dc6232287870bbc36837
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-09-19 23:25:19 -0700

    styling complete

--- src/components/home/Info.jsx
+++ src/components/home/Info.jsx
@@ -3,9 +3,9 @@ import Social from "./Social";
 const Info = () => {
   return (
     <div className="home_data">
-      <h1 className="title">Hi, I'm Shiyao Zhai</h1>
-      <h3 className="subtitle">Software Developer</h3>
-      <p className="description">
+      <h1 className="home title">Hi, I'm Shiyao Zhai</h1>
+      <h3 className="home subtitle">Software Developer</h3>
+      <p className="home description">
         I am a full stack web designer with a passion for bringing ideas to life
         through code.
       </p>
